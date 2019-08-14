c4232b979c6d19500274005b4985850166838e4b
DaWoCX <42966977+DaWoCX@users.noreply.github.com>
2019-08-14 16:05:39 +0200

    First Commit! Setup

---
+++ Dwarf_Code_Gen/Dwarf_Code_Gen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwarf_Code_Gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Button_Grid_Generator();
+            Console.ReadLine();
+        }
+
+        public static void Button_Grid_Generator()
+        {
+            for(int i = 0; i < 50; i++)
+            {
+                //Console.WriteLine("#region " + (i + 1).ToString() + " Row");
+                Console.WriteLine("<!-- " + (i + 1).ToString() + ". Row ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->");
+                for(int x = 0;  x < 50; x++)
+                {
+
+                    Console.WriteLine("<Button x:Name=\"GRID_" + (i < 10 ? "0" : "") + i.ToString() +"I" + (x < 10 ? "0" : "") + x.ToString() + "\" Grid.ColumnSpan=\"1\" Content=\"\" Grid.Column=\"" + i.ToString() + "\" " +
+                                        "Grid.Row=\"" + x.ToString() + "\" Width=\"11\" Height=\"11\"/>");
+                }
+                //Console.WriteLine("#endregion");
+            }
+        }
+    }
+}
---
+++ Dwarfs/Dwarfs/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+
+namespace Dwarfs
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow mainWindow = new MainWindow();
+
+            System.Windows.Forms.Screen s1 = System.Windows.Forms.Screen.AllScreens[0];
+            Rectangle r1 = s1.WorkingArea;
+            mainWindow.Top = r1.Top;
+            mainWindow.Left = r1.Left;
+            mainWindow.Show();
+
+        }
+    }
+
+}
